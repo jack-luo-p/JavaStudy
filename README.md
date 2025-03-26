87f3f840d06dc2e13b33198af7dce497315f48b3
jack-luo-p <3049212272@qq.com>
2025-03-26 21:05:36 +0800

    add泛型

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ java基础学习
 ##package
 ###Day1
     InitConstruct: 构造器初始化
+###Day2 泛型
---
+++ luopeng/src/com/java/Day2/Generic.java
@@ -0,0 +1,12 @@
+package com.java.Day2;
+
+/**
+ * @ClassDescription:泛型类
+ * @Author:LuoPeng
+ * @Create:2025/3/23 16:37
+ * @Param: <T>泛型标识 - 类型形参
+ **/
+public class Generic<T> {
+    private T key;
+    
+}
